8a0dbb4913010c6f9a4d1a6b4472515515070129
HelberCarvalhoRodrigues <123906822+HelberCarvalhoRodrigues@users.noreply.github.com>
2023-02-12 16:11:20 -0300

    clicknopapel

--- Assets/Scripts/PlayerClickControl.cs
+++ Assets/Scripts/PlayerClickControl.cs
@@ -2,13 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class PlayerClickControl : MonoBehaviour
 {
     public GameObject objectHide; //Objeto a ser escondido.
+    public UnityEvent onClick;
+
 
     private void OnMouseDown() //Evento do click do player sobre o Objeto
     {
+        onClick.Invoke();
         if (objectHide.activeSelf)
         {
             objectHide.SetActive(false); //Visibilidade do objeto (false)
